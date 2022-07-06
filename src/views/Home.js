0c66fc7e9286a05179f1b9dfe56538b74d9f03da
Arian <arianrdcas@gmail.com>
2022-07-06 17:53:10 -0400

    estilos ok

--- src/views/Home.js
+++ src/views/Home.js
@@ -25,80 +25,87 @@ const Home = () => {
 
 
   return (
-    <>
-      <h1 className="container text-center text-danger mt-5">“No. No lo intentes. Hazlo, o no lo hagas, pero no lo intentes.”</h1>
-      <h5 className="container pe-5 text-end text-danger">Maestro Yoda</h5>
-      <div className="container">
-        <h1 className="mt-5 text-danger">Personajes <GiAlienBug/></h1>
-        <div className="d-flex">
-            {store.personajes.map((personajes, id) => {
-              return (
-                  <div className="col-6 col-md-4">  
-                    <div key={id} className="card" style={{ width: "18rem" }}>
-                      <div className="card-body">
-                        <h5 className="card-title">Nombre: {personajes.name}</h5>
-                        <p className="card-text">Gender: {personajes.gender}</p>
-                        <p className="card-text">Hair color: {personajes.hair_color}</p>
-                        <p className="card-text">Eye color: {personajes.eye_color}</p>
-                        <Link
-                          to="/detalle_personaje"
-                          type="button"
-                          className="btn btn-outline-primary"
-                        >
-                          Detalles
-                        </Link>
-                        <button
-                          type="button"
-                          className="btn btn-outline-warning float-end"
-                          onClick={()=>addFavorito(personajes.name)}
-                        >
-                          <IoMdHeartEmpty/>
-                        </button>
-                      </div>
-                    </div>
-                  </div>   
-              );
-            })}
-          </div>
+    <>  
+      <div className="container">  
+        <h1 className="text-center text-danger mt-5">“No. No lo intentes. Hazlo, o no lo hagas, pero no lo intentes.”</h1>
+        <h5 className="pe-5 text-end text-danger">Maestro Yoda</h5>
       </div>
-      <div className="container">
-        <h1 className="mt-5 text-danger">Planetas <IoPlanetSharp/></h1>
-        <div className="d-flex">
-            {store.planetas.map((planetas, id) => {
-              return (
-                  <div className="col-6 col-md-4">  
-                    <div key={id} className="card" style={{ width: "18rem" }}>
-                      <div className="card-body">
-                        <h5 className="card-title">Nombre: {planetas.name}</h5>
-                        <p className="card-text">Periodo de Rotacion: {planetas.rotation_period}</p>
-                        <p className="card-text">Gravedad: {planetas.gravity}</p>
-                        <p className="card-text">Habitantes: {planetas.population}</p>
-                        <Link
-                          to="/detalle_personaje"
-                          type="button"
-                          className="btn btn-outline-primary"
-                        >
-                          Detalles
-                        </Link>
-                        <button
-                          type="button"
-                          className="btn btn-outline-warning float-end"
-                        >
-                          <IoMdHeartEmpty/>
-                        </button>
+        <div className="container">
+          <div className="row">
+            <h1 className="mt-5 mb-4 text-danger">Personajes <GiAlienBug/></h1>
+            <div className="d-flex flex-nowrap py-1">
+              {store.personajes.map((personajes, id) => {
+                return (
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-3">  
+                      <div key={id} className="card" style={{ width: "18rem" }}>
+                        <div className="card-body">
+                          <h5 className="card-title">Nombre: {personajes.name}</h5>
+                          <p className="card-text">Gender: {personajes.gender}</p>
+                          <p className="card-text">Hair color: {personajes.hair_color}</p>
+                          <p className="card-text">Eye color: {personajes.eye_color}</p>
+                          <Link
+                            to="/detalle_personaje"
+                            type="button"
+                            className="btn btn-outline-primary"
+                          >
+                            Detalles
+                          </Link>
+                          <button
+                            type="button"
+                            className="btn btn-outline-warning float-end"
+                            onClick={()=>addFavorito(personajes.name)}
+                          >
+                            <IoMdHeartEmpty/>
+                          </button>
+                        </div>
                       </div>
-                    </div>
-                  </div>   
-              );
-            })}
+                    </div>   
+                );
+              })}
+            </div>
           </div>
-      </div>
-      <div className="container mb-3">
-        <h1 className="mt-5 text-danger">Naves <SiSkyliner/></h1>
-        <div className="d-flex">
-            {store.naves.map((naves, id) => {
-              return (
-                  <div className="col-6 col-md-4">  
+        </div>
+        <div className="container">
+          <div className="row">
+            <h1 className="mt-5 mb-4 text-danger">Planetas <IoPlanetSharp/></h1>
+            <div className="d-flex flex-nowrap py-1">
+              {store.planetas.map((planetas, id) => {
+                return (
+                    <div className="col-12 col-md-6 col-lg-4 col-xl-3">  
+                      <div key={id} className="card" style={{ width: "18rem" }}>
+                        <div className="card-body">
+                          <h5 className="card-title">Nombre: {planetas.name}</h5>
+                          <p className="card-text">Periodo de Rotacion: {planetas.rotation_period}</p>
+                          <p className="card-text">Gravedad: {planetas.gravity}</p>
+                          <p className="card-text">Habitantes: {planetas.population}</p>
+                          <Link
+                            to="/detalle_personaje"
+                            type="button"
+                            className="btn btn-outline-primary"
+                          >
+                            Detalles
+                          </Link>
+                          <button
+                            type="button"
+                            className="btn btn-outline-warning float-end"
+                          >
+                            <IoMdHeartEmpty/>
+                          </button>
+                        </div>
+                      </div>
+                    </div>   
+                );
+              })}
+            </div>
+          </div>
+        </div>
+      <div className="container mb-5">
+        <div className="row">
+            <h1 className="mt-5 mb-4 text-danger">Naves <SiSkyliner/></h1>
+            <div className="d-flex flex-nowrap py-1">
+              {store.naves.map((naves, id) => {
+                return (
+                  <div className="col-12 col-md-6 col-lg-4 col-xl-3">
                     <div key={id} className="card"  style={{ width: "18rem" }}>
                       <div className="card-body">
                         <h5 className="card-title">Nombre: {naves.name}</h5>
@@ -120,11 +127,12 @@ const Home = () => {
                         </button>
                       </div>
                     </div>
-                  </div>   
-              );
-            })}
-          </div>
-      </div>
+                  </div> 
+                );
+              })}
+            </div>
+        </div>
+      </div> 
     </>
   );
 };
